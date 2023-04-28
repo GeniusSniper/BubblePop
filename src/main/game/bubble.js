cb470da46b94b55452f3d1b10baa9b64bde028ea
GeniusSniper <tiwei61342@gmail.com>
2023-04-28 15:07:33 -0400

    remove bubble after clicking

--- src/main/game/bubble.js
+++ src/main/game/bubble.js
@@ -7,12 +7,13 @@
 // }
 
 const Bubble = () => {
-    let element = document.createElement("div");
+    const element = document.createElement("div");
     element.innerHTML = "";
     element.style.border = "1px solid black";
     element.style.borderRadius = '50%';
     element.style.height = '25px';
     element.style.width = '25px';
+    element.style.background = 'black';
     return element;
 };
 
--- src/main/game/game.js
+++ src/main/game/game.js
@@ -1,8 +1,10 @@
 import Bubble from './bubble';
 
 const Game = () => {
-    let bubbles = Bubble();
-    document.querySelector('body').appendChild(bubbles);
+    const parent = document.querySelector('body');
+    const bubbles = Bubble();
+    bubbles.addEventListener("click", e => parent.removeChild(bubbles));
+    parent.appendChild(bubbles);
 };
 
 export default Game;
